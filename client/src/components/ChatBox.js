2663c71f930703fcb1906e054525fef98b4a8476
Seeker223 <baduntobi2024@gmail.com>
2024-03-21 13:20:15 +0100

    ChatBox component updated

---
+++ client/src/components/ChatBox.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {Avatar, Image} from 'antd'
+
+export default function ChatBoxReceiver({avatar, user, message}) {
+    return (
+     <div style={{display: 'flex'}, justifyContent: 'flex', flexDirection: 'row'}>
+        <Avatar
+        
+        size={50}
+        src={<Image
+             src={avatar}
+             style={{
+                objectFit: 'cover',
+                width:45,
+                height:45,
+                borderRadius: "100%"
+             }}  
+             preview={false}
+        />}
+
+   />
+        
+   <p style={{padding:10, backgroundColor:'#dcf8c6', borderRadius:10, maxwidth:"60%"}}>
+        <strong style={{fontSize:13}}>
+              {user}
+        </strong> <br></br>
+        {message}
+
+   </p>   
+
+     <div/>
+    )
+}
+
+
+export default function ChatBoxSender() {
+    return (
+        <div>ChatBox<div/>
+    )
+}
